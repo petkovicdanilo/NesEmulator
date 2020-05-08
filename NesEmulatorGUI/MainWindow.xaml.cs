bcbd7624c16d9e40885d49c9f3508c527a0c944b
Danilo Petkovic <petkovicdanilo97@gmail.com>
2020-05-08 23:54:39 +0200

    Add rendering of background in GUI
    Adds basic implementation of rendering background in GUI application.

--- NesEmulatorGUI/MainWindow.xaml.cs
+++ NesEmulatorGUI/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using NesLib;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,13 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Drawing;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using NesLib.Utils;
+using NesLib.Devices.PpuEntities.Registers;
 
 namespace NesEmulatorGUI
 {
@@ -22,18 +27,59 @@ namespace NesEmulatorGUI
     public partial class MainWindow : Window
     {
         private Nes nes;
+        private int counter = 0;
+
         public MainWindow()
         {
             InitializeComponent();
             nes = new Nes();
 
-            Status.Text = nes.PrintCpu();
+            RenderOptions.SetBitmapScalingMode(NesScreen, BitmapScalingMode.NearestNeighbor);
+            RenderOptions.SetEdgeMode(NesScreen, EdgeMode.Aliased);
+
+            CompositionTarget.Rendering += RenderNesScreen;
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private int _frameCounter = 0;
+        private Stopwatch _stopwatch = new Stopwatch();
+
+        protected void RenderNesScreen(object sender, EventArgs e)
         {
-            nes.Clock();
-            Status.Text = nes.PrintCpu();
+            if (_frameCounter++ == 0)
+            {
+                // Starting timing.
+                _stopwatch.Start();
+            }
+
+            do
+            //{
+            //for (int i = 0; i < 100000; ++i)
+            {
+                nes.Clock();
+            }
+            //}
+            while (!nes.FrameComplete);
+            NesScreen.Source = nes.Screen();
+            nes.FrameComplete = false;
+
+            if (counter == 15)
+            {
+                using (FileStream stream5 = new FileStream("screen.bmp", FileMode.Create))
+                {
+                    PngBitmapEncoder encoder5 = new PngBitmapEncoder();
+                    encoder5.Frames.Add(BitmapFrame.Create(nes.Screen()));
+                    encoder5.Save(stream5);
+                }
+            }
+            counter++;
+
+            // Determine frame rate in fps (frames per second).
+            long frameRate = (long)(_frameCounter / this._stopwatch.Elapsed.TotalSeconds);
+            if (frameRate > 0)
+            {
+                // Update elapsed time, number of frames, and frame rate.
+
+            }
         }
     }
 }
